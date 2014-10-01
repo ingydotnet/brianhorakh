e251b19b71496cebae69a7aa5c012ba41fcfe8ce
Ingy döt Net <ingy@ingy.net>
2014-10-02 01:43:49 +0200

    add a test

---
+++ test.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use Pegex::Parser;
+use Pegex::Grammar;
+use Pegex::Tree::Wrap;
+use IO::All;
+use XXX;
+
+my $grammar = io->file('new-style.pgx')->all;
+
+my $parser = Pegex::Parser->new(
+    grammar => Pegex::Grammar->new(text => $grammar),
+    receiver => Pegex::Tree::Wrap->new,
+    debug => 1,
+);
+
+XXX $parser->parse("set  ' world'; apply --append;");
